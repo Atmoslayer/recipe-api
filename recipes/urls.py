8f58672b99a017c658b46c9a5a30801fbb2d99e0
Atmoslayer <ddsomdim@gmail.com>
2024-01-29 18:11:02 +0300

    Rename basenames of endpoints

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import UpdateRecipeProductViewSet, IncreaseRecipeProductViewSet
 
 router = routers.DefaultRouter()
-router.register(r'add_product_to_recipe', UpdateRecipeProductViewSet, basename='items')
-router.register(r'cook_recipe', IncreaseRecipeProductViewSet, basename='increase_products')
+router.register('add_product_to_recipe', UpdateRecipeProductViewSet, basename='add_products')
+router.register('cook_recipe', IncreaseRecipeProductViewSet, basename='increase_products')
 
 urlpatterns = router.urls
